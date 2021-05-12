2f1ba782296c0a1af1a1d58411e76c0512b3b63b
지운 김 <kkimj@hanyang.ac.kr>
2021-05-12 23:05:11 +0900

    upload emotion model

--- emo_book_flutter/lib/models/book_model.dart
+++ emo_book_flutter/lib/models/book_model.dart
@@ -1,4 +1,42 @@
 class Book {
+  // id
+  String asin, isbn;
+
+  // detail
   String title;
-  num id;
+  String image;
+
+  // review
+  String review;
+
+  /*
+reviews
+add-to-shelf,
+read,
+review actions
+
+book attributes:
+title,
+isbn,
+asin,
+graph of similar books
+*/
 }
+
+/* example
+"image": ["https://images-na.ssl-images-amazon.com/images/I/71eG75FTJJL._SY88.jpg"], 
+"overall": 5.0, 
+"vote": "2", 
+"verified": True, 
+"reviewTime": "01 1, 2018", 
+"reviewerID": "AUI6WTTT0QZYS", 
+"asin": "5120053084", 
+"style": {
+	"Size:": "Large", 
+	"Color:": "Charcoal"
+	}, 
+"reviewerName": "Abbey", 
+"reviewText": "I now have 4 of the 5 available colors of this shirt... ", 
+"summary": "Comfy, flattering, discreet--highly recommended!", 
+"unixReviewTime": 1514764800
+*/
