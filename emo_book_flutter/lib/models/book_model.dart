4cc661bb6062b2bd2dc5f0fb8f87517dde66c029
지운 김 <kkimj@hanyang.ac.kr>
2021-05-12 23:35:10 +0900

    update Book model

--- emo_book_flutter/lib/models/book_model.dart
+++ emo_book_flutter/lib/models/book_model.dart
@@ -7,36 +7,16 @@ class Book {
   String image;
 
   // review
-  String review;
-
-  /*
-reviews
-add-to-shelf,
-read,
-review actions
-
-book attributes:
-title,
-isbn,
-asin,
-graph of similar books
-*/
+  List<BookReview> bookReviews;
 }
 
-/* example
-"image": ["https://images-na.ssl-images-amazon.com/images/I/71eG75FTJJL._SY88.jpg"], 
-"overall": 5.0, 
-"vote": "2", 
-"verified": True, 
-"reviewTime": "01 1, 2018", 
-"reviewerID": "AUI6WTTT0QZYS", 
-"asin": "5120053084", 
-"style": {
-	"Size:": "Large", 
-	"Color:": "Charcoal"
-	}, 
-"reviewerName": "Abbey", 
-"reviewText": "I now have 4 of the 5 available colors of this shirt... ", 
-"summary": "Comfy, flattering, discreet--highly recommended!", 
-"unixReviewTime": 1514764800
-*/
+class BookReview {
+  String review_text;
+  String reviwer_name;
+  String review_summary;
+  String review_time;
+  String review_image;
+  num review_overall;
+  num review_upvote;
+  num review_downvote;
+}
