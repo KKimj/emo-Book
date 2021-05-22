c1f03fb32ce6b031093dca51d87ca68fda37e056
지운 김 <kkimj@hanyang.ac.kr>
2021-05-23 00:09:05 +0900

    update for style

--- emo_book_flutter/lib/styles.dart
+++ emo_book_flutter/lib/styles.dart
@@ -2,3 +2,39 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Font {}
+
+class Buttons {
+  static ButtonStyle get emotion_pressed => ButtonStyle(
+        backgroundColor:
+            MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+        // shape: MaterialStateProperty<OutlinedBorder>
+        textStyle: MaterialStateProperty.all<TextStyle>(
+            TextStyle(fontSize: 10.0, color: Colors.white)),
+        elevation: MaterialStateProperty.all<double>(1.0),
+        // padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+        //   EdgeInsets.fromLTRB(10, 10, 10, 10),
+        // ),
+      );
+  static ButtonStyle get emotion_unpressed => ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+        // shape: MaterialStateProperty<OutlinedBorder>
+        textStyle: MaterialStateProperty.all<TextStyle>(
+            TextStyle(fontSize: 10.0, color: Colors.blue)),
+        elevation: MaterialStateProperty.all<double>(4.0),
+        // padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+        //   EdgeInsets.fromLTRB(10, 10, 10, 10),
+        // ),
+      );
+
+  static ButtonStyle get emotion_pressed_evlvated => ElevatedButton.styleFrom(
+      elevation: 1.0,
+      primary: Colors.lightBlueAccent,
+      onPrimary: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)));
+
+  static ButtonStyle get emotion_unpressed_evlvated => ElevatedButton.styleFrom(
+      elevation: 4.0,
+      primary: Colors.white,
+      onPrimary: Colors.blue,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)));
+}
--- emo_book_flutter/lib/views/widgets/emotion_widgets.dart
+++ emo_book_flutter/lib/views/widgets/emotion_widgets.dart
@@ -4,6 +4,9 @@ import 'package:get/get.dart';
 //controllers
 import 'package:emo_book_flutter/controllers/emotion_controller.dart';
 
+// styles
+import 'package:emo_book_flutter/styles.dart';
+
 class EmotionButton extends StatelessWidget {
   late final String emotion_title;
 
@@ -13,21 +16,23 @@ class EmotionButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<EmotionController>(
       builder: (_) {
-        return ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                primary: EmotionController.to.emotion_seletions[emotion_title]!
-                    ? Colors.lightBlueAccent
-                    : Colors.white,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
-            onPressed: () {
-              EmotionController.to.toggle_emotion(emotion_title);
-              print(emotion_title +
-                  ' is ${EmotionController.to.emotion_seletions[emotion_title]}');
-              Get.snackbar(emotion_title,
-                  ' is ${EmotionController.to.emotion_seletions[emotion_title]}');
-            },
-            child: Text(emotion_title));
+        return Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: ElevatedButton(
+              style: EmotionController.to.emotion_seletions[emotion_title]!
+                  ? Buttons.emotion_pressed_evlvated
+                  : Buttons.emotion_unpressed_evlvated,
+              onPressed: () {
+                EmotionController.to.toggle_emotion(emotion_title);
+                print(emotion_title +
+                    ' is ${EmotionController.to.emotion_seletions[emotion_title]}');
+                Get.snackbar(emotion_title,
+                    ' is ${EmotionController.to.emotion_seletions[emotion_title]}');
+              },
+              child: Text(
+                emotion_title,
+              )),
+        );
       },
     );
   }
