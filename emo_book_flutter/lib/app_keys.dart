57819fdcb2f3d32aed0101ba07ad22a9163e39b1
지운 김 <kkimj@hanyang.ac.kr>
2021-05-23 14:51:08 +0900

    update for app key list

--- emo_book_flutter/lib/app_keys.dart
+++ emo_book_flutter/lib/app_keys.dart
@@ -1,4 +1,8 @@
 class AppKeys {
   static String firebaseApiKey = '';
   static String firestoreProjectId = '';
+  static String kakaoRestApiKey = '';
+  static String kakaoRestNativeKey = '';
+  static String kakaoRestJsKey = '';
+  static String kakaoRestAdminKey = '';
 }
