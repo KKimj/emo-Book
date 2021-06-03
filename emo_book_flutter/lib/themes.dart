893b8cf238009b7c840062bb2275994718f00120
지운 김 <kkimj@hanyang.ac.kr>
2021-06-04 00:05:51 +0900

    update themes.dart for sync with font styles #4

--- emo_book_flutter/lib/themes.dart
+++ emo_book_flutter/lib/themes.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
+// styles
+import 'package:emo_book_flutter/styles.dart';
+
 ThemeData EmoLightTheme = ThemeData.light().copyWith(
   primaryColor: Color(0xFF264653),
   accentColor: Color(0xFF2a9d8f),
   appBarTheme: AppBarTheme(color: Color(0xFF264653)),
   textTheme: TextTheme(
-    headline6: TextStyle(color: Colors.black),
-    subtitle1: TextStyle(color: Colors.black),
-    subtitle2: TextStyle(color: Colors.black),
+    headline1: TextStyles.h1,
+    headline2: TextStyles.h2,
+    headline3: TextStyles.h3,
+    headline4: TextStyles.h4,
+    headline5: TextStyles.h5,
+    headline6: TextStyles.h6,
+    subtitle1: TextStyles.subtitle1,
+    subtitle2: TextStyles.subtitle2,
+    bodyText1: TextStyles.body1,
+    bodyText2: TextStyles.body2,
+    button: TextStyles.button,
+    caption: TextStyles.caption,
   ),
   textButtonTheme: TextButtonThemeData(),
   outlinedButtonTheme: OutlinedButtonThemeData(),
@@ -24,10 +36,19 @@ ThemeData EmoDarkTheme = ThemeData.dark().copyWith(
   accentColor: Color(0xFF2a9d8f),
   appBarTheme: AppBarTheme(color: Color(0xFF264653)),
   textTheme: TextTheme(
-    headline6: TextStyle(color: Colors.white),
-    subtitle1: TextStyle(color: Colors.white),
-    subtitle2: TextStyle(color: Colors.white),
-  ),
+    headline1: TextStyles.h1,
+    headline2: TextStyles.h2,
+    headline3: TextStyles.h3,
+    headline4: TextStyles.h4,
+    headline5: TextStyles.h5,
+    headline6: TextStyles.h6,
+    subtitle1: TextStyles.subtitle1,
+    subtitle2: TextStyles.subtitle2,
+    bodyText1: TextStyles.body1,
+    bodyText2: TextStyles.body2,
+    button: TextStyles.button,
+    caption: TextStyles.caption,
+  ).apply(bodyColor: Colors.white, displayColor: Colors.white),
   textButtonTheme: TextButtonThemeData(),
   outlinedButtonTheme: OutlinedButtonThemeData(),
   elevatedButtonTheme: ElevatedButtonThemeData(
