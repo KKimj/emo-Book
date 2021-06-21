17a43e5c64258554fe151f95b528bdde5b097578
지운 김 <kkimj@hanyang.ac.kr>
2021-06-21 16:04:55 +0900

    chore : fix chore error (#15)

--- emo_book_flutter/test/widget_test.dart
+++ emo_book_flutter/test/widget_test.dart
@@ -5,15 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:emo_book_flutter/main_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:emo_book_flutter/main.dart';
+// import 'package:emo_book_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MainScaffold());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
