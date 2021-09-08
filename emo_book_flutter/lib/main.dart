e373c89fd6f6c075e53c444e0870c9f5c7e4c2eb
Jiun Kim <40026920+KKimj@users.noreply.github.com>
2021-09-08 10:45:13 +0900

    Fix Initialized

--- emo_book_flutter/lib/main.dart
+++ emo_book_flutter/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+
+// Packages
+import 'package:firebase_core/firebase_core.dart';
+
+// Main App
 import 'package:emo_book_flutter/main_scaffold.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MainScaffold());
 }
