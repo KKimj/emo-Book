a4754e11e8624adbd88fcb143e2d16d26d171010
Jiun Kim <40026920+KKimj@users.noreply.github.com>
2021-10-04 23:03:16 +0900

    Update main scaffold

--- emo_book_flutter/lib/main_scaffold.dart
+++ emo_book_flutter/lib/main_scaffold.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'core_packages.dart';
 
+// Bindings
+import 'package:emo_book_flutter/bindings/initialbinding.dart';
+
 // Controllers
 import 'package:emo_book_flutter/controllers/app_controller.dart';
 import 'package:emo_book_flutter/controllers/book_controller.dart';
@@ -13,6 +16,7 @@ import 'package:emo_book_flutter/views/book_page/book_page.dart';
 import 'package:emo_book_flutter/views/home_page/home_page.dart';
 import 'package:emo_book_flutter/views/user_page/user_page.dart';
 import 'package:emo_book_flutter/views/login_page/login_page.dart';
+import 'package:emo_book_flutter/views/signup_page/signup_page.dart';
 
 // Dev
 import 'package:emo_book_flutter/controllers/debug_controller.dart';
@@ -29,31 +33,24 @@ class MainScaffold extends StatelessWidget {
       //
       GetPage(name: '/login', page: () => LoginPage()),
       //
+      GetPage(name: '/signup', page: () => SignupPage()),
+
       GetPage(name: '/user', page: () => UserPage()),
+      GetPage(name: '/user/:uid', page: () => UserPage()),
       //
       GetPage(name: '/debug', page: () => DebugPage()),
     ];
 
     return GetMaterialApp(
       title: 'emo-book :: 감정기반 도서 추천 서비스',
-      home: HomePage(),
-      theme: EmoLightTheme,
+      onInit: () {
+        InitialBinding()
+            .dependencies()
+            .then((value) => AppController.to.setInitialized());
+      },
+      initialRoute: '/',
+      theme: EmoDarkTheme,
       getPages: _getPages,
-      initialBinding: AppBinding(),
     );
-  }
-}
-
-class AppBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.put<AppController>(AppController(), permanent: true);
-    Get.put<BookController>(BookController());
-    Get.put<EmotionController>(EmotionController());
-
-    Get.put<UserController>(UserController());
-
-    Get.lazyPut(() => DebugController());
-    // Get.lazyPut(() => UserController());
   }
 }
